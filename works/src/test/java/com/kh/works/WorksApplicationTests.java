9dff2c4805885812720d37c8b3288f8b2307d4c6
DESKTOP-VVUATKV\geuna <geuna_1019@naver.com>
2024-06-16 23:12:39 +0900

    스프링부트 파일 만들었습니다 설정이랑 다 해놨습니다

---
+++ works/src/test/java/com/kh/works/WorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.works;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
