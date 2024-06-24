e2f0807b20d83fbfca6754f0ee3ab9c6c10c4f09
yerinsong05 <yerinsong05@gmail.com>
2024-06-24 19:58:18 +0900

    todo 작성 기능 구현

--- works/src/main/java/com/kh/works/todo/TodoDao.java
+++ works/src/main/java/com/kh/works/todo/TodoDao.java
@@ -3,6 +3,8 @@ package com.kh.works.todo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class TodoDao {
@@ -11,4 +13,11 @@ public class TodoDao {
     public int write(TodoVo vo) {
         return mapper.write(vo);
     }
+
+    public int todoManager(TodoManagerVo mvo) {
+        return mapper.todoManager(mvo);
+    }
+
+
+
 }
