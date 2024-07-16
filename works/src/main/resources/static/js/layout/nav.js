273871b58bc90569a3ddacbab9e3ff755776fc01
DESKTOP-DNN7P3H\user1 <geuna_1019@naver.com>
2024-07-16 16:15:35 +0900

    nav css 작업 완료

---
+++ works/src/main/resources/static/js/layout/nav.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector("#modalBtn");
+btn.addEventListener('click' , ()=>{
+    const modal = document.querySelector("#modal");
+    if(modal.style.display === "block"){
+        modal.style.display = "none"
+    }else{
+        modal.style.display="block";
+    }
+    
+})
