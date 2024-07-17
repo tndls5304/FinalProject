cb7deda034bb5aa1a6a671f25d022b029477d7f6
yerinsong05 <yerinsong05@gmail.com>
2024-07-17 20:35:25 +0900

    할일 홈, 상세조회 프론트 수정(하다가 기절할뻔함

--- works/src/main/resources/static/js/todo/detail.js
+++ works/src/main/resources/static/js/todo/detail.js
@@ -7,32 +7,36 @@ function getTodoDetail(todoNo) {
       console.log(data);
       const detail = document.querySelector("#detail");
 
-      // const title = document.querySelector("#detail");
-
       let str = "";
-      str += `<div id='todoNo'class='hidden-no'>${data[0].todoNo}</div>`;
-      str += `<div><span name='changeTitle' id='title'>${data[0].title}</span></div>`;
-      str += `<div><span name='changeContent' id='content'>${data[0].content}</span></div>`;
-      str += `<div id='endDate'>작성일: ${data[0].createDate}</div>`;
-      str += `<div id='endDate'>기한:  ${data[0].endDate}</div>`;
-      str += `<div id='empName'>요청자: <button class="empBtn" onclick="getEmpDetail(${data[0].todoEmpNo});">${data[0].todoEmpName}</button></div>`;
+      str += `<div id='todoNo' class='hidden-no'>${data[0].todoNo}</div>`;
+      str += `<div class='todo-content'>`;
+      str += `<span name='changeTitle' id='title'>${data[0].title}</span>`;
+      str += `<span name='changeContent' id='content'>${data[0].content}</span>`;
+      str += `</div>`;
+      str += `<div id='create-date'>작성일: ${data[0].createDate}</div>`;
+      str += `<div id='end-date'>기한: ${data[0].endDate}</div>`;
 
-      //완료되지 않은 할일에만 버튼 생기게
-      if (data[0].completedYn !== "Y") {
-        str += `<button class='comBtn' onclick='complete(${data[0].todoNo});'>완료하기</button>`;
-      }
+      str += `<div class='request'>요청자: <button onclick="getEmpDetail(${data[0].todoEmpNo});">${data[0].todoEmpName}</button></div>`;
 
-      //담당자만 여러명이라 for문
-      str += `<div id='todoManagerNo'>담당자 :`;
+      str += `<div class='manager'>담당자: `;
       for (let i = 0; i < data.length; i++) {
-        str += `<button class="managerBtn" onclick="getEmpDetail(${data[i].todoManagerNo});">${data[i].todoManagerName}</button>`;
+        str += `<button onclick="getEmpDetail(${data[i].todoManagerNo});">${data[i].todoManagerName}</button>`;
       }
-      str += "</div>";
+      str += `</div>`;
 
+      // 수정 및 삭제 버튼을 같은 div에 넣기
+      str += `<div class='action-btn'>`;
       str += `<button class='editBtn' onclick='editBtn(${data[0].todoNo});'>수정하기</button>`;
-      str += `<button class=delToBtn onclick='delTodo(${data[0].todoNo});'>삭제하기</button>`;
+      str += `<button onclick='delTodo(${data[0].todoNo});'>삭제하기</button>`;
 
-      detail.innerHTML = str;
+      // 완료되지 않은 할일에만 버튼 생기게
+      if (data[0].completedYn !== "Y") {
+        str += `<button id='comBtn' onclick='complete(${data[0].todoNo});'>완료하기</button>`;
+      }
+      str += `</div>`; // action-btn div 종료
+
+      // 기존 내용을 제거하고 새로운 내용을 삽입
+      detail.innerHTML = str; // 수정: str을 사용하여 내용을 업데이트
     },
     error: function (err) {
       console.error("상세조회 ajax 실행중 에러", err);
